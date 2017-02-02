9629b30641436e1a10d456bfb38c59ff5f5f83be
Martsynyuk <terr83@gmail.com>
2017-02-02 16:44:19 +0200

    create Employee model

--- module/Application/src/Model/Employee.php
+++ module/Application/src/Model/Employee.php
@@ -34,4 +34,82 @@ class Employee
      */
     private $user;
 
+    /**
+     * @var string
+     * @ORM\Column(name="full_name", length=511, type="string", nullable=true)
+     */
+    private $fullName;
+
+    /**
+     * @var string
+     * @ORM\Column(name="mobile_phone", length=215, type="string", nullable=true)
+     */
+    private $mobilePhone;
+
+    /**
+     * @var string
+     * @ORM\Column(name="landline_phone", length=215, type="string", nullable=true)
+     */
+    private $landlinePhone;
+
+    /**
+     * @var string
+     * @ORM\Column(name="email", length=511, type="string", nullable=true)
+     */
+    private $email;
+
+    /**
+     * @var boolean
+     * @ORM\Column(name="experience", length=1, type="boolean") // ?
+     */
+    private $experience;
+
+    /**
+     * @var int
+     * @ORM\Column(name="area_around", length=10 type="integer",  nullable=true)
+     */
+    private $areaAround;
+
+    /**
+     * @var boolean
+     * @ORM\Column(name="driving_licence", length=1, type="boolean") // ?
+     */
+    private $drivingLicence;
+
+    /**
+     * @var boolean
+     * @ORM\Column(name="car_available", length=1, type="boolean") // ?
+     */
+    private $carAvailable;
+
+    /**
+     * @var string
+     * @ORM\Column(name="contract_type", length=511, type="string", nullable=true)
+     */
+    private $contractType;
+
+    /**
+     * @var int
+     * @ORM\Column(name="weekly_hours_available", length=10, type="integer",  nullable=true)
+     */
+    private $weeklyHoursAvailable;
+
+    /**
+     * @var date
+     * @ORM\Column(name="startDate", type="datetime",  nullable=true)
+     */
+    private $startDate;
+
+    /**
+     * @var string
+     * @ORM\Column(name="comments", type="text",  nullable=true)
+     */
+    private $comments;
+
+    /**
+     * @var
+     * @ORM\Column(name="hourly_rate", precision=2, type="decimal",  nullable=true)
+     */
+    private $hourlyRate;
+
 }
