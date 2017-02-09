f4990f9b0b497b403b38cdcc831196aac003f7f8
vitalii.krushelnytskyi <vitalii.krushelnytskyi@gmail.com>
2017-02-09 16:07:54 +0200

    Language generator added

---
+++ module/Application/locale/generate.php
@@ -0,0 +1,81 @@
+<?php
+
+include __DIR__ . '/../../../vendor/autoload.php';
+
+$localeDir = __DIR__;
+
+$dirs = [
+    __DIR__ . '/../'
+];
+
+$toTranslate = [];
+
+$translatorApiUri = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
+$translatorKey = 'trnsl.1.1.20170209T132126Z.979b34845c1404ed.ff6376236a7cb7d04ca03d8267e05fb8cc6fb12c';
+
+$locales = (include __DIR__ . '/../config/module.config.php')['locales'];
+
+while (false === empty($dirs)) {
+
+    $dir = array_shift($dirs);
+
+    foreach (getFiles($dir) as $innerFile) {
+        if (true === is_dir($innerFile)) {
+            $dirs[] = $innerFile;
+            continue;
+        } else {
+            preg_match_all('/\-\>translate\(\'(.*)\'\)/mui', file_get_contents($innerFile), $matches);
+            $toTranslate = array_unique(array_merge($toTranslate, $matches[1]));
+        }
+    }
+
+}
+
+$client = new \GuzzleHttp\Client();
+
+foreach ($locales as $locale => $language) {
+
+    list($lang) = explode('_', $locale);
+
+    $pattern = 'msgid ""
+msgstr ""
+"Project-Id-Version: \n"
+"Report-Msgid-Bugs-To: \n"
+"Last-Translator: \n"
+"Language-Team: %s\n"
+
+';
+
+    file_put_contents($localeDir . '/' .$locale . '.po', sprintf($pattern, $language));
+
+    foreach ($toTranslate as $word) {
+        $result = json_decode((string)$client->get(
+            $translatorApiUri . '?key=' . $translatorKey
+            . '&text=' . htmlentities($word)
+            . '&lang=en-' . $lang
+        )->getBody())->text[0];
+
+        file_put_contents($localeDir . '/' .$locale . '.po', "msgid \"$word\"\nmsgstr \"$result\"\n\n", FILE_APPEND);
+
+    }
+
+}
+
+/**
+ * @param $root
+ * @return array
+ */
+function getFiles($root)
+{
+    return array_filter(array_map(
+        function ($path) use ($root) {
+            return $root . '/' . $path;
+        },
+        array_filter(
+            scandir($root),
+            function ($path) {
+                return $path !== '.' && $path !== '..';
+            }
+        )
+    ));
+}
