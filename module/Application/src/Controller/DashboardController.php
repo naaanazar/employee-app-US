f7ec4bedb19a3a1133574edde26f2b15e673d7c7
Nazar Korchevsky <naaa.nazar@gmail.com>
2017-02-09 13:13:02 +0200

    add models

--- module/Application/src/Controller/DashboardController.php
+++ module/Application/src/Controller/DashboardController.php
@@ -5,6 +5,8 @@ namespace Application\Controller;
 use Application\Back\Paginator\Adapter\Doctrine;
 use Application\Model\Area;
 use Application\Model\RegisterKey;
+use Application\Model\Contract;
+use Application\Model\WeeklyHours;
 use Doctrine\ORM\OptimisticLockException;
 use Doctrine\ORM\ORMInvalidArgumentException;
 use Zend\Paginator\Paginator;
@@ -107,6 +109,106 @@ class DashboardController extends AbstractController
         } else {
             $paginator = new Paginator(
                 new Doctrine(RegisterKey::class)
+            );
+
+            $paginator->setItemCountPerPage(20);
+            $paginator->setCurrentPageNumber($this->params('page', 1));
+
+            $view = new ViewModel();
+            $view->setVariables(
+                [
+                    'paginator' => $paginator
+                ]
+            );
+
+            return $view;
+        }
+    }
+
+    public function contractAction()
+    {
+        if (true === $this->getRequest()->isPost()
+            && true === $this->getRequest()->isXmlHttpRequest()
+            && null !== $this->getRequest()->getPost('name')
+        ){
+
+            $name = $this->getRequest()->getPost('name');
+            $code  = str_replace(" ", "-", preg_replace('/\s\s+/', ' ', $name));
+
+            $json = new JsonModel();
+            $contract = new Contract();
+            $contract->setName($name);
+            $contract->setCode($code);
+
+            try {
+                $this->getEntityManager()->persist($contract);
+                $this->getEntityManager()->flush();
+
+                $json->setVariable(
+                    'redirect',
+                    $this->url()->fromRoute('dashboard', ['action' => 'contract']));
+            } catch (ORMInvalidArgumentException $exception) {
+                $json->setVariable('message', 'Invalid data to save contract type');
+            } catch (OptimisticLockException $exception) {
+                $json->setVariable('message', 'Can not save contract type to database');
+            }
+
+            return $json;
+
+        } else {
+
+            $paginator = new Paginator(
+                new Doctrine(Contract::class)
+            );
+
+            $paginator->setItemCountPerPage(20);
+            $paginator->setCurrentPageNumber($this->params('page', 1));
+
+            $view = new ViewModel();
+            $view->setVariables(
+                [
+                    'paginator' => $paginator
+                ]
+            );
+
+            return $view;
+        }
+    }
+
+    public function weeklyHoursAction()
+    {
+        if (true === $this->getRequest()->isPost()
+            && true === $this->getRequest()->isXmlHttpRequest()
+            && null !== $this->getRequest()->getPost('value')
+        ){
+
+            $value = $this->getRequest()->getPost('value');
+            $intValue = preg_replace('/[^\-\d]*(\-?\d*).*/','$1',$value) * 3600;
+
+            $json = new JsonModel();
+            $WH = new WeeklyHours();
+            $WH->setIntValue($intValue);
+            $WH->setValue($value);
+
+            try {
+                $this->getEntityManager()->persist($WH);
+                $this->getEntityManager()->flush();
+
+                $json->setVariable(
+                    'redirect',
+                    $this->url()->fromRoute('dashboard', ['action' => 'weekly-hours']));
+            } catch (ORMInvalidArgumentException $exception) {
+                $json->setVariable('message', 'Invalid data to save area around');
+            } catch (OptimisticLockException $exception) {
+                $json->setVariable('message', 'Can not save area around to database');
+            }
+
+            return $json;
+
+        } else {
+
+            $paginator = new Paginator(
+                new Doctrine(WeeklyHours::class)
             );
 
             $paginator->setItemCountPerPage(20);
