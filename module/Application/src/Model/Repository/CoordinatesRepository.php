df234afb7004f6da11a7c4ed28647a5d87d5b990
vitalii.krushelnytskyi <vitalii.krushelnytskyi@gmail.com>
2017-02-10 18:05:08 +0200

    Agregator improved

--- module/Application/src/Model/Repository/CoordinatesRepository.php
+++ module/Application/src/Model/Repository/CoordinatesRepository.php
@@ -4,7 +4,6 @@ namespace Application\Model\Repository;
 
 use Application\Back\Map\Agregator;
 use Application\Model\Coordinates;
-use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\EntityRepository;
 
 /**
@@ -16,31 +15,16 @@ class CoordinatesRepository extends EntityRepository
 
     /**
      * @param Coordinates $coordinates
-     * @param $range
      * @return array
      */
-    public function getCoordinatesInRange(Coordinates $coordinates, $range)
+    public function getCoordinatesInRange(Coordinates $coordinates)
     {
-        $agregator = new Agregator($coordinates);
-
-        $criteria = new Criteria();
-        $criteria
-            ->where($criteria->expr()->gt('longitude', $agregator->getMinimumLongitude($range)))
-            ->andWhere($criteria->expr()->lt('longitude', $agregator->getMaximumLongitude($range)))
-            ->andWhere($criteria->expr()->gt('latitude', $agregator->getMinimumLatitude($range)))
-            ->andWhere($criteria->expr()->lt('latitude', $agregator->getMaximumLatitude($range)));
-
-        $coordinatesModels = $this->matching($criteria)->toArray();
+        $agregator = new Agregator($coordinates);;
+        $coordinatesModels = $this->findAll();
 
         foreach ($coordinatesModels as &$coordinatesModel) {
-
             /** @var Coordinates $coordinatesModel */
-            $maximumRange = max(
-                $agregator->getDistance($coordinatesModel) > $range,
-                $coordinatesModel->getEmployee()->getAreaAround()->getIntValue()
-            );
-
-            if ($maximumRange > $range) {
+            if ($agregator->getDistance($coordinatesModel) <= $coordinatesModel->getEmployee()->getAreaAround()->getIntValue()) {
                 unset($coordinatesModel);
             }
         }
