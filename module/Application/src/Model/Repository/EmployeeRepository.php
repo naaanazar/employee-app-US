bac73e21bed7bb7aad56972fe0060da1068238ba
Nazar <naaa.nazar@gnail.com>
2017-03-06 14:58:31 +0200

    fix bugs

--- module/Application/src/Model/Repository/EmployeeRepository.php
+++ module/Application/src/Model/Repository/EmployeeRepository.php
@@ -90,7 +90,7 @@ class EmployeeRepository extends EntityRepository
             //->addExpression('contains', 'zip', $params['zip'])
             ->addExpression('eq', 'carAvailable', $params['car_available'])
             ->addExpression('eq', 'drivingLicence', $params['driving_license'])
-            ->addExpression('eq', 'areaAround', $this->getEntityManager()->getRepository(Area::class)->find($params['area_around']));
+            ->addExpression('lte', 'areaAround', $this->getEntityManager()->getRepository(Area::class)->find($params['area_around']));
 
         if (true === isset($params['lastSearch'])) {
             $this->addExpression('gt', 'created', $params['lastSearch']);
